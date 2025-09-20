91274b9875bcafd9302e752f65829dff995ff986
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-09-20 16:58:03 +0300

    extract scanner package

--- cmd/tcp-scanner/main.go
+++ cmd/tcp-scanner/main.go
@@ -1,54 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"sort"
+	"github.com/cmczk/tcp-scanner/scanner"
 )
 
 const host = "scanme.nmap.org"
-const portsCount = 65535
+const maxPort = 65535
 
 func main() {
-	ports := make(chan int, 100)
-	results := make(chan int)
-	var openports []int
-
-	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
-	}
-
-	go func() {
-		for i := 1; i <= portsCount; i++ {
-			ports <- i
-		}
-	}()
-
-	for range portsCount {
-		port := <-results
-		if port != 0 {
-			openports = append(openports, port)
-		}
-	}
-
-	close(ports)
-	close(results)
-	sort.Ints(openports)
-	for _, port := range openports {
-		fmt.Printf("port %d open\n", port)
-	}
-}
-
-func worker(ports, results chan int) {
-	for p := range ports {
-		address := fmt.Sprintf("[%s]:%d", host, p)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			results <- 0
-			continue
-		}
-
-		conn.Close()
-		results <- p
-	}
+	s := scanner.New(host, nil, maxPort)
+	s.Scan()
 }
---
+++ scanner/scanner.go
@@ -0,0 +1,70 @@
+package scanner
+
+import (
+	"fmt"
+	"net"
+	"sort"
+)
+
+type Scanner struct {
+	host    string
+	minPort int
+	maxPort int
+}
+
+func New(host string, minPort *int, maxPort int) *Scanner {
+	min := 1
+	if minPort != nil {
+		min = *minPort
+	}
+
+	return &Scanner{
+		host:    host,
+		minPort: min,
+		maxPort: maxPort,
+	}
+}
+
+func (s *Scanner) Scan() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go s.worker(ports, results)
+	}
+
+	go func() {
+		for i := s.minPort; i <= s.maxPort; i++ {
+			ports <- i
+		}
+	}()
+
+	for range s.maxPort {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("port %d open\n", port)
+	}
+}
+
+func (s *Scanner) worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("[%s]:%d", s.host, p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+
+		conn.Close()
+		results <- p
+	}
+}
